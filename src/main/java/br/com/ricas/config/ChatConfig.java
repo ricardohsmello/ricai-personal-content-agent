6d2af4dbfebc21e712da321af5d3bf9d9af82a01
Ricardo Mello <ricardohsmello@gmail.com>
2026-08-08 13:31:15 -0300

    Adding spring ai + chatmessage

---
+++ src/main/java/br/com/ricas/config/ChatConfig.java
@@ -0,0 +1,15 @@
+package br.com.ricas.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatConfig {
+
+	@Bean
+	public ChatClient chatClient(OpenAiChatModel openAiChatModel) {
+		return ChatClient.builder(openAiChatModel).build();
+	}
+}
--- src/main/java/br/com/ricas/web/ContentChatController.java
+++ src/main/java/br/com/ricas/web/ContentChatController.java
@@ -1,6 +1,8 @@
 package br.com.ricas.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chat")
 public class ContentChatController {
 
-	@GetMapping
-	public String getOk() {
-		return "Testing";
+	private final ChatClient chatClient;
+
+	ContentChatController(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
+
+	@PostMapping
+	public String chat(@RequestBody String message) {
+		return chatClient.prompt(message)
+				.call()
+				.content();
 	}
 }
